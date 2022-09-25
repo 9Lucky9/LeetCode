8d573071c545d22e2004f37b241e47e872d018aa
Павел Евстигнеев <pro1912@gmail.com>
2022-09-26 00:40:26 +0300

    Add string builder solution

--- LeetCode/Easy/PalindromeNumber.cs
+++ LeetCode/Easy/PalindromeNumber.cs
@@ -1,15 +1,34 @@
-﻿namespace LeetCode.Easy
+﻿using System.Text;
+
+namespace LeetCode.Easy
 {
     /// <summary>
     /// https://leetcode.com/problems/palindrome-number/
     /// </summary>
-    public static class PalindromeNumber
+    public class PalindromeNumber
     {
-        public static bool IsPalindrome(int x)
+        /// <summary>
+        /// Inbuilt methods.
+        /// </summary>
+        public bool IsPalindrome(int x)
         {
             if (x.ToString() == string.Concat(x.ToString().Reverse()))
                 return true;
             return false;
+        }
+
+        /// <summary>
+        /// Using String Builder.
+        /// </summary>
+        public bool IsPalindromeStringBuilder(int x)
+        {
+            var originalString = x.ToString();
+            var stringBuilder = new StringBuilder();
+            for(int i = originalString.Length-1; i >= 0; i--)
+            {
+                stringBuilder.Append(originalString[i]);
+            }
+            return originalString.Equals(stringBuilder.ToString());
         }
     }
 }
